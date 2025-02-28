a26df379c13d2ec4b816f98e3f28b0a6ef29bd85
Guilherme D. <guilhermerd6@hotmail.com>
2025-02-28 14:17:46 -0300

    feat: updated dependencies and added function to query RAG

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,12 +1,12 @@
-from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_ollama import OllamaEmbeddings
 
 def get_embedding_function():
     """Gets an embedding function from Langchain.
-    This function returns an embedding function provided by Amazon, but it can also be done
-    using local embedding models.
+    This function returns an embedding function provided by Ollama, but it can also be done
+    using other local embedding models or online embedding providers like Amazon Bedrock.
 
     Returns:
-        _type_: Embedding function provided by Bedrock (Amazon).
+        _type_: Embedding function provided by Ollama.
     """
     embeddings = OllamaEmbeddings(
         model="nomic-embed-text"
